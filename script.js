a19d0ea80a724c460c66fc2683e66cb3831d709e
Neel Madhav Dogra <ndogra@Neels-MacBook-Pro.local>
2024-11-07 12:16:41 -0700

    update template

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 
+let currentViz = 1;
+const totalViz = 5;
+
+document.getElementById('nextBtn').addEventListener('click', () => {
+    document.getElementById(`viz${currentViz}`).classList.remove('active');
+
+    currentViz = currentViz < totalViz ? currentViz + 1 : 1;
+
+    document.getElementById(`viz${currentViz}`).classList.add('active');
+});
